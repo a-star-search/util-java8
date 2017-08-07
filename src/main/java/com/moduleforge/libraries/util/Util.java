1440fb48fb623386d524fd338e7d576b2d5c59a9
diego <diego.pardo.martin@gmail.com>
2017-08-07 14:45:14 +0200

    initial version of my utilities project

---
+++ src/main/java/com/moduleforge/libraries/util/Util.java
@@ -0,0 +1,31 @@
+package com.moduleforge.libraries.util;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Util {
+
+   public static <T, R> List<R> apply(Collection<T> coll, Function<? super T, ? extends R> mapper) {
+      return coll.stream().map(mapper).collect(Collectors.toList());
+   }
+
+   public static int[] toPrimitive(final Integer[] array) {
+      if (array == null) {
+         throw new IllegalArgumentException();
+      }
+      if (array.length == 0) {
+         return new int[0];
+      }
+      final int[] result = new int[array.length];
+      for (int i = 0; i < array.length; i++) {
+         result[i] = array[i].intValue(); 
+      }
+      return result;
+   }
+   
+   public static <T> void print(Collection<T> coll) {
+      coll.forEach((elem)->System.out.println(elem));
+   }
+}
